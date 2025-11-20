5c548079b7ef0f91bca85eb62302f7c459b3fd6c
ashdebiz <31121010+ashdebiz@users.noreply.github.com>
2025-11-20 17:26:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -102,7 +102,7 @@ def get_docs():
 # SIDEBAR
 # =============================================
 with st.sidebar:
-    st.image("https://d1csarkz8obe9u.cloudfront.net/posterpreviews/9e87ee02b72ac2273edd3aae9cc0a8af_screen.jpg?ts=1763630610&", width=80)
+    st.image("https://e7.pngegg.com/pngimages/985/662/png-clipart-fruits-veggies-more-matters-vegetable-healthy-diet-vegetable-fruits-amp.png", width=80)
     st.markdown("<h3 style='color:white;text-align:center;margin-top:10px;'>FAMA STANDARD</h3>", unsafe_allow_html=True)
     page = st.selectbox("Menu", ["Halaman Utama", "Admin Panel"], label_visibility="collapsed")
 
